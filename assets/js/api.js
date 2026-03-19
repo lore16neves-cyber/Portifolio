8992e6f79eb3d20e4a67e6d510ca859e7f2ea2d3
Lorena Nerves <jadergomesc@gmail.com>
2026-03-19 20:38:30 -0300

    Icluindo raw

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 async function fetchProfileData() {
 
-    const url = 'https://raw.githubusercontent.com/lore16neves-cyber/Portifolio/5c4a56af0c7c4dc4ee3faafb30eb0de295ad6349/assets/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/lore16neves-cyber/Portifolio/5c4a56af0c7c4dc4ee3faafb30eb0de295ad6349/assets/data/profile.json?token=GHSAT0AAAAAADYI6M4KX2DXPUE5DO3VXH2Q2N4Q24A';
 
     const fetching = await fetch(url);
     return await fetching.json();
